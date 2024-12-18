da1a646a185490f3153c741b872d243082065608
Caleb San Lucas <cssan@puce.edu.ec>
2024-12-18 17:44:44 -0500

    laboratorio 8

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:pokemon>/", views.pokemon, name="pokemon"),
+    path("", views.index, name="index"), 
+    path("<int:pokemon_id>/", views.pokemon, name="pokemon"),
 ]
