79350f2086aadcbf9a9917b3b097a04f9e07ca30
Caleb Sanlucas <cssan@puce.edu.ec>
2025-01-17 23:36:39 -0500

    Laboratorio 9

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+
 from .models import Pokemon,Trainer
+from pokedex.forms import PokemonForm
+
 
 def index(request):
     pokemons = Pokemon.objects.all()
@@ -23,3 +29,39 @@ def trainer_details(request, trainer_id):
         'trainer': trainer
     }
     return HttpResponse(template.render(context, request))
+
+@login_required
+def add_pokemon(request):
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+
+    else:
+        form = PokemonForm()
+
+    return render(request, 'pokemon_form.html', {'form': form})
+
+
+def edit_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(pk = pokemon_id)
+    if request.method == "POST":
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+
+    else:
+        form = PokemonForm(instance=pokemon)
+
+    return render(request, 'pokemon_form.html', {'form': form})
+
+
+def delete_pokemon(request, pokemon_id):
+    pokemon = Pokemon.objects.get(pk = pokemon_id)
+    pokemon.delete()
+    return redirect('pokedex:index')
+
+class CustomLoginView(LoginView):
+    template_name = "login_form.html"
